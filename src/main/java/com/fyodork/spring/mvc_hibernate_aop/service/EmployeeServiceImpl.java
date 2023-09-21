eab478fa246a5ea660388ad178a0b4567334c5ba
svenka <svenkagrip@gmail.com>
2023-09-21 22:17:03 +0800

    Add employee-info

--- src/main/java/com/fyodork/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/fyodork/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -4,8 +4,10 @@ import com.fyodork.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.fyodork.spring.mvc_hibernate_aop.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
@@ -13,7 +15,14 @@ public class EmployeeServiceImpl implements EmployeeService{
     private EmployeeDAO employeeDAO;
 
     @Override
+    @Transactional
     public List<Employee> getAllEmployees() {
         return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
     }
 }
