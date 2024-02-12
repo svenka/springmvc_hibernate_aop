189c89bb32a66eaba5e51d24f53e43377d1d8417
svenka <svenkagrip@gmail.com>
2024-02-12 19:07:43 +0800

    Add aop Around advice

--- src/main/java/com/fyodork/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/fyodork/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -7,6 +7,9 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @Aspect
 public class MyLoggingAspect {
@@ -17,10 +20,12 @@ public class MyLoggingAspect {
     {
         MethodSignature methodSignature= (MethodSignature) proceedingJoinPoint.getSignature();
         String methodName= methodSignature.getName();
-        System.out.println("Begin of method:"+methodName);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM.dd.yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Begin of method :"+dtf.format(now)+" "+methodName);
         Object targetMethodResult=proceedingJoinPoint.proceed();
 
-        System.out.println("end of method:"+methodName);
+        System.out.println("end of method:"+dtf.format(now)+" "+methodName);
         return targetMethodResult;
     }
 }
